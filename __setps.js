63cf35267b3ae1c31f2af23562eb5b18f433f352
unknown <technicalsalma@gmail.com>
2022-04-21 15:34:24 +0600

    my frist node

---
+++ __setps.js
@@ -0,0 +1,4 @@
+/**
+ *1. create a folder (manually or mkdir)
+ *2. open command line to that folder
+ */
---
+++ index.js
@@ -0,0 +1,55 @@
+const express =require('express');
+const cors = require('cors')
+const app = express();
+const port =process.env.PORT || 5000;
+
+app.use(cors())
+app.use(express.json());
+
+app.get('/', (req, res) =>{
+    res.send('Look Mama! You now, I can code Node now!!! ')
+});
+
+
+const users = [
+    {id: 1, name: 'sabana', email:'sabana@gmail.com', phone:'01788888'},
+    {id: 2, name: 'sabnur', email:'sabnur@gmail.com', phone:'01799999999'},
+    {id: 3, name: 'bobita', email:'bobita@gmail.com', phone:'01788888'},
+    {id: 4, name: 'mousumi', email:'mousumi@gmail.com', phone:'017888888'},
+    {id: 5, name: 'kobita', email:'kobita@gmail.com', phone:'017888888'},
+    {id: 6, name: 'purnima', email:'purnima@gmail.com', phone:'017888888'},
+    {id: 7, name: 'popi', email:'popi@gmail.com', phone:'017888888'}
+]
+
+app.get('/users', (req, res) =>{
+   if(req.query.name){
+       const search= req.query.name.toLowerCase();
+       const matched = users.filter(user=>user.name.toLowerCase().includes(search))
+       res.send(matched)
+   }
+    res.send(users);
+})
+
+app.get('/user/:id', (req, res) =>{
+    console.log(req.params);
+    const id = parseInt(req.params.id);
+    const user = users.find(u => u.id === id);
+    res.send(user)
+})
+
+app.post('/user', (req, res) =>{
+    console.log('request',req.body);
+    const user = req.body;
+    user.id = users.length + 1;
+    users.push(user)
+    res.send(user)
+})
+
+app.get('/flower',(req, res) =>{
+    res.send(['rose', 'daliha','chinarose'])
+})
+
+
+app.listen(port, () =>{
+    console.log('Listening to port', port);
+})
